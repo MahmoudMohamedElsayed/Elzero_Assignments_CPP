121768658b8eff0e5d918698c4d88ce97cfaa7e1
Mahmoud Elsayed <166971531+MahmoudMohamedElsayed@users.noreply.github.com>
2024-10-02 17:19:23 +0300

    Add files via upload

---
+++ week 4- assignments/week 4 assignment-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Assign The Values
+    char a = '1';
+    char b = '1';
+    char c = '1';
+
+    a = 10;
+    b = 10;
+    c = 99;
+
+    // Do Not Edit
+    cout << b * c + a << "\n"; // 1000
+    return 0;
+}
---
+++ week 5- assignments/week 5 assignment-6.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int age = 40;
+  int points = 100;
+
+  if (age > 18 && points > 50 && sizeof(age) == 4)
+    cout << "Age Data Is 4 Bytes \n"
+         << "Age Is Ok  \n"
+         << "Points Is Ok";
+
+  return 0;
+}
